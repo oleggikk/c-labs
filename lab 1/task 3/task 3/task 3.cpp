bef035a06636fa5cea7407c8d701342e63d45aa1
John Doe <oleg.y07.10.03@gmail.com>
2022-11-03 23:13:23 +0300

    task 3

---
+++ lab 1/task 3/task 3/task 3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	double a;
+	double b;
+	double c;
+	double S;
+
+	std::cout << "Enter fist base: ";
+	std::cin >> a;
+	std::cout << "Enter second side: ";
+	std::cin >> b;
+	
+	c = pow((pow(a, 2) + pow(b, 2)), 0.5);
+	S = 0.5 * a * b;
+
+	std::cout << "Hypotenuse = " << c << std::endl;
+	std::cout << "Square = " << S << std::endl;
+	
+	return 0;
+}
