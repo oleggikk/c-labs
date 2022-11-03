eb6acf2ad2366958e59f0967dcfa8a7912dc63d7
John Doe <oleg.y07.10.03@gmail.com>
2022-11-03 22:48:30 +0300

    Добавьте файлы проекта.

---
+++ lab 1/task 1/task 1/task 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	double b1;
+	double b2;
+	double h;
+	double S;
+
+	std::cout << "Enter fist base: ";
+	std::cin >> b1;
+	std::cout << "Enter second base: ";
+	std::cin >> b2;
+	std::cout << "Enter height: ";
+	std::cin >> h;
+	S = (b1 + b2) / 2 * h;
+	std::cout << "Square = " << S << std::endl;
+
+	return 0;
+}
