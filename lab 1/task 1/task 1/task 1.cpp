aaa395911c1d5819a6e49201d722895b7574fb38
John Doe <oleg.y07.10.03@gmail.com>
2022-11-03 23:18:53 +0300

    comments

--- lab 1/task 1/task 1/task 1.cpp
+++ lab 1/task 1/task 1/task 1.cpp
@@ -4,17 +4,17 @@ int main()
 {
 	double b1;
 	double b2;
-	double h;
+	double h; #Объявляем переменные
 	double S;
 
 	std::cout << "Enter fist base: ";
-	std::cin >> b1;
+	std::cin >> b1; #Вводим значении b1
 	std::cout << "Enter second base: ";
-	std::cin >> b2;
+	std::cin >> b2; #Вводим значении b2
 	std::cout << "Enter height: ";
-	std::cin >> h;
-	S = (b1 + b2) / 2 * h;
-	std::cout << "Square = " << S << std::endl;
+	std::cin >> h; #Вводим значении h
+		S = (b1 + b2) / 2 * h; #Считаем площадь трапеции
+		std::cout << "Square = " << S << std::endl; #Выводим ответ
 
 	return 0;
 }
--- lab 1/task 3/task 3/task 3.cpp
+++ lab 1/task 3/task 3/task 3.cpp
@@ -9,15 +9,15 @@ int main()
 	double S;
 
 	std::cout << "Enter fist base: ";
-	std::cin >> a;
+	std::cin >> a; #Вводим катет
 	std::cout << "Enter second side: ";
-	std::cin >> b;
+	std::cin >> b; #Вводим катет
 	
-	c = pow((pow(a, 2) + pow(b, 2)), 0.5);
-	S = 0.5 * a * b;
+	c = pow((pow(a, 2) + pow(b, 2)), 0.5); #Считаем площадь, используем метод pow для возведения в степень
+	S = 0.5 * a * b; #Считаем площадь
 
 	std::cout << "Hypotenuse = " << c << std::endl;
-	std::cout << "Square = " << S << std::endl;
+	std::cout << "Square = " << S << std::endl; #Вывод
 	
 	return 0;
 }
